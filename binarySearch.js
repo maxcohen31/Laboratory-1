26ed213b843d7902e7dcc349e4c5a343c6411693
maxcohen31 <eman_93@hotmail.it>
2023-11-04 14:17:16 +0100

    exercise

---
+++ binarySearch.js
@@ -0,0 +1,40 @@
+/*
+    Ricerca binaria 
+*/
+
+
+function binary_search(arr, target)
+{
+    let position = -1;
+    // array start
+    let start = 0;
+    // array end
+    let end = arr.length - 1;
+
+
+    while(start <= end)
+    {   
+        // get middle element
+        let mid = start + ((end - start) / 2);
+
+        if(arr[mid] === target)
+        {
+            return mid;
+        }
+        // If target is to the right of our mid then increase left pointer
+        if(arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+
+        // If target is to the left of our mid then decrease right pointer
+        if(arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+    }
+    return position;
+}
+
+let x = [4, 5, 6, 12, 23, 55, 79];
+console.log(binary_search(x, 23));
