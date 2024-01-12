6c4df4242362595cf3505e8f0d71b326301e8be5
maxcohen31 <eman_93@hotmail.it>
2024-01-12 12:37:16 +0100

    fix: wrong output

--- replace.js
+++ replace.js
@@ -16,9 +16,13 @@ function replace(arr, target, replacement)
 
     for(let i = 0; i < arr.length; i++)
     {
-        if(arr[i] === target)
+        if(arr[i] != target)
         {
-            arr[i] = replacement;
+            newArr.push(arr[i]);
+        }
+        else
+        {
+            newArr[i] = replacement;
         }
     }
     return newArr;
