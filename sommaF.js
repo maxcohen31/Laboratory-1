f0e313de21d19e53880fdac99b6032fea4a31528
maxcohen31 <eman_93@hotmail.it>
2024-01-12 14:42:53 +0100

    exercise

--- sommaF.js
+++ sommaF.js
@@ -1,31 +1,28 @@
 /*
-    Si scriva una funzione 
-    che prende come parametro una funzione 
-    che preso un intero restituisce un valore numerico. 
-    restituisce una funzione che prende come parametro un array di interi, e
-    restituisce la somma dei valori restituiti da 
+    Si scriva una funzione sommaF(f) che prende come parametro una funzione f
+    che preso un intero restituisce un valore numerico.
+    SommaF(f) restituisce una funzione che prende come parametro un array di interi, 
+    e restituisce la somma dei valori restituiti da f
     su ogni elemento dell'array.
+
+    Esempi:
+
+    sommaF(x=>x*x)([1,2,3,4]) restituisce 30
+
+    sommaF(x=>2*x)([1,2,3,4]) restituisce 20
+
+    sommaF(Math.sqrt)([1,2,3,4]) restituisce 6.146264369941973
+
 */
 
-function sommaF(f)
+function sommaF(f) 
 {
     return function(arr) 
     {
-        let somma = 0;
-
-        for(let i = 0; i < arr.length; i++)
-        {
-            somma += f(arr[i])
-        }
-        
-        return somma;
-    };
-
+        return  arr.reduce((accum, curr) => (accum + f(curr)), 0) 
+    }   
 }
 
-console.log(sommaF(x => 2*x)([1, 2, 3, 4]))
-
-// sommaF(x=>x*x)([1,2,3,4]) restituisce 30
-
-
-console.log(sommaF(Math.sqrt)([1,2,3,4])) // restituisce 6.146264369941973
+console.log(sommaF(x=>x*x)([1, 2, 3, 4])) 
+console.log(sommaF(x=>2*x)([1, 2, 3, 4])) 
+console.log(sommaF(Math.sqrt)([1, 2, 3, 4])) 
