60abe98899896fa7b25bdbd87374a8a5a0073495
maxcohen31 <eman_93@hotmail.it>
2024-02-10 12:25:39 +0100

    exercise

--- printBST.js
+++ printBST.js
@@ -1,35 +1,38 @@
 /*
     Si scriva una funzione JS stampaC(T) con T un Albero Binario di Ricerca (ABR).
 
-
-
     L'albero binario di ricerca è rappresentato come visto a lezione, 
     i cui nodi sono oggetti con tre proprietà: val (contenente un valore intero), 
     sx (un riferimento al nodo sinistro) e dx (un riferimento al nodo destro).
 
-
-
     La funzione deve restituire un array contenente in ordine crescente i valori dei nodi di T 
     che abbiano almeno un figlio a valore pari. Non è possibile usare nessuna funzione di ordinamento.
+
+
+    NOTA: Un ABR è un albero binario in cui il valore di ogni nodo t è più piccolo dei 
+    valori di tutti i nodi presenti nel sotto-albero destro di t, 
+    ed è più grande dei valori di tutti i nodi presenti nel sotto-albero destro di t.
+
 */
 
-
-function stampaC(T)
+function stampaC(node, result = []) 
 {
-    let result = [];
-    
-    if(T == null) { return []; }
-
-    if(T.sx && T.sx.val % 2 === 0 || T.dx && T.dx.val) 
+    if (node) 
     {
-        result.push(T.val);
+        if (node.val % 2 === 0) 
+        {
+        result.push(node.val);
+        }
+        stampaC(node.sx, result)
+        stampaC(node.dx, result)
     }
-    
-    return [...result, ...stampaC(T.sx), ...stampaC(T.dx)]
-    
-    
+  return result
 }
 
 
-let albe = {val: 1, sx: {val: 2, sx: {val: 4}, dx: {val: 5}}, dx: {val: 3, dx: {val: 6}}};
-console.log(stampaC(albe))
+let tree = {val:7,sx:{val: 4, sx: {val: 3}, dx: {val:12, sx: {val: 4, dx:{val:8}, sx:{val: 2}}}}, dx:{val: 11, dx: {val: 18}, sx: {val:3, sx: {val: 2}}}};
+
+let tree2 = {val: 2};
+
+console.log(stampaC(tree2))
+console.log(stampaC(tree))
