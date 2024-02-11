d7433d237c2184b3686106a68579d9061d45fe81
maxcohen31 <eman_93@hotmail.it>
2024-02-11 10:15:53 +0100

    fix: exercise text

--- BCD.js
+++ BCD.js
@@ -1,5 +1,16 @@
 /*
-    La rappresentazione Binary-Coded Decimal (BCD) si basa sull'idea di rappresentare ogni cifra di un numero decimale attraverso la sua rappresentazione binaria, ma lasciando come moltiplicatore la base 10 fra le cifre. Poiché le cifre decimali esprimono valori fra 0 e 9, sono necessari 4 bit per rappresentare una cifra decimale (con 3 bit potremmo avere al massimo 0-7). Con un byte, ovvero 8 bit, potremo dunque rappresentare in BCD numeri decimali fra 0 e 99. Questa rappresentazione -- curiosa dal punto di vista matematico -- ha alcuni vantaggi pratici, per esempio è molto facile usare i bit di un numero codificato in BCD come linee di controllo di un display a 7 segmenti (trovate una simulazione interattiva del circuito elettronico relativo alla pagina https://www.sgart.it/IT/elettro/decoder-bcd-7-segmenti/post).
+    La rappresentazione Binary-Coded Decimal (BCD) si basa sull'idea di 
+    rappresentare ogni cifra di un numero decimale attraverso la sua rappresentazione binaria, 
+    ma lasciando come moltiplicatore la base 10 fra le cifre. Poiché le cifre decimali esprimono valori fra 0 e 9, 
+    sono necessari 4 bit per rappresentare una cifra decimale 
+    (con 3 bit potremmo avere al massimo 0-7). 
+    Con un byte, ovvero 8 bit, potremo dunque rappresentare in BCD numeri decimali fra 0 e 99. 
+    Questa rappresentazione -- curiosa dal punto di vista matematico -- ha alcuni vantaggi pratici, 
+    per esempio è molto facile usare i bit di un numero codificato 
+    in BCD come linee di controllo di un display a 7 segmenti 
+    (trovate una simulazione interattiva del circuito elettronico relativo alla pagina 
+    https://www.sgart.it/IT/elettro/decoder-bcd-7-segmenti/post).
+
 */
 
 function bcd(n)
